d30ae1ef6a935c6d322598febd0df0093502d37f
Anton Bäckström <anton.backstrom@gmail.com>
2018-10-13 20:59:49 +0200

    Update OpenFiles.py

--- OpenFiles.py
+++ OpenFiles.py
@@ -5,29 +5,30 @@ Created on Sat Oct 13 18:18:21 2018
 @author: Anton
 """
 
-"""
+# Opening textfile in different ways
+
 f=open('test.txt','r')
 print("f.read(), everything: ")
 print(f.read())
 f.close()
 
 print("\n")
-with open('test.txt','r') as g: #context manager. need sno close statement
+with open('test.txt','r') as g: #context manager. needs no close statement
     print("g.readLines(): ")
     print(g.readlines())        
      
 print("\n")    
-with open('test.txt','r') as g: #context manager. need sno close statement
+with open('test.txt','r') as g:
     print("g.read 20 first characters in the file: ")
     print(g.read(20))     
 
 print("\n")
-with open('test.txt','r') as g: #context manager. need sno close statement
+with open('test.txt','r') as g: 
     print("g.readLine(5), 5 first characters in first line: ")
     print(g.readline(5))         
 
 print("\n")    
-with open('test.txt','r') as g: #context manager. need sno close statement
+with open('test.txt','r') as g:
     print("g.read, everything: ") 
     print(g.read()) # reads everything
       
@@ -35,7 +36,8 @@ with open('test.txt','r') as g: #context manager. need sno close statement
 print("\n")    
 print("Check if closed: ")
 print(g.closed)
-"""
+
+# Opening picture
 
 with open('pic.jpg','rb') as rf:    
     with open('pic.jpg','wb') as wf:    
