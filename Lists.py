e56da17a7c8d644b7e39a48b3949a02662ff3c13
Anton Bäckström <antonba@student.chalmers.se>
2018-10-15 21:01:58 +0200

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -35,5 +35,5 @@ print(new_list)
 
 # Sets are faster than list and tuples in general. Can only have one of each element in a set,
 # and they are not ordered
-# some methods, intersection, union, difference
+# some methods: intersection, union, difference
 
